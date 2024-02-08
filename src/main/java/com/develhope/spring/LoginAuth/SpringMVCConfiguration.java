2b5babf656a47e9b7d265186ee03c4828b429808
Valeria Parise <parisevaleria@gmail.com>
2024-02-08 16:29:01 +0100

    MVC configurer

--- src/main/java/com/develhope/spring/LoginAuth/SpringMVCConfiguration.java
+++ src/main/java/com/develhope/spring/LoginAuth/SpringMVCConfiguration.java
@@ -2,11 +2,22 @@ package com.develhope.spring.LoginAuth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-
 public class SpringMVCConfiguration implements WebMvcConfigurer {
     @Autowired
-    private CustomerInterceptor clientInterceptor;
+    private CustomerInterceptor customerInterceptor;
+    @Autowired
+    private AdminInterceptor adminInterceptor;
+    @Autowired
+    private SellerInterceptor sellerInterceptor;
+
+//    @Override
+    public void addInterceptor(InterceptorRegistry registry){
+        registry.addInterceptor(customerInterceptor).addPathPatterns("/customer/**");
+        registry.addInterceptor(adminInterceptor).addPathPatterns("/admin/**");
+        registry.addInterceptor(sellerInterceptor).addPathPatterns("/seller/**");
+    }
 }
