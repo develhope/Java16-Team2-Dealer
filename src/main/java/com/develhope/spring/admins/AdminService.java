2d4dd579eceb23e8a9c2e29daad010b2d9d41166
Valeria Parise <parisevaleria@gmail.com>
2024-02-22 18:24:27 +0100

    update and delete vehicle

--- src/main/java/com/develhope/spring/admins/AdminService.java
+++ src/main/java/com/develhope/spring/admins/AdminService.java
@@ -12,13 +12,12 @@ import com.develhope.spring.vehicle.forrental.VehicleForRentalRepository;
 import com.develhope.spring.vehicle.forsale.StatusSale;
 import com.develhope.spring.vehicle.forsale.VehicleForSale;
 import com.develhope.spring.vehicle.forsale.VehicleForSaleRepository;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
-//Modificare un veicolo
-//Cancellare un veicolo
 //Cambiare lo stato di un veicolo
 //Creare un noleggio per un utente
 //Cancellare un noleggio per un utente
@@ -54,7 +53,7 @@ public class AdminService {
     @Autowired
     private RentalOrderRepository rentalOrderRepository;
 
-    //    Aggiungere un veicolo
+    //Aggiungere un veicolo
     public VehicleForSale addVehicleSale(VehicleEntity vehicleSale) {
         VehicleForSale vehicle = new VehicleForSale();
         vehicle.setStatus(vehicle.getStatus());
@@ -77,109 +76,131 @@ public class AdminService {
         return vehicle;
     }
 
+    //Modificare un veicolo
+    @SneakyThrows
+    public VehicleEntity updateVehicle(VehicleEntity vehicleEntity, Long vehicleId) {
+        VehicleEntity newVehicle = new VehicleEntity();
+        if (vehicleForSaleRepository.existsById(vehicleId)) {
+            newVehicle = vehicleForSaleRepository.findById(vehicleId).get();
+        } else if (vehicleForRentalRepository.existsById(vehicleId)) {
+            newVehicle = vehicleForRentalRepository.findById(vehicleId).get();
+        }
+        return newVehicle;
+    }
+
+
+    //Cancellare un veicolo
+    @SneakyThrows
+    public void deleteVehicle(Long vehicleId) {
+        if (vehicleForSaleRepository.existsById(vehicleId)) {
+            vehicleForSaleRepository.deleteById(vehicleId);
+        } else if (vehicleForRentalRepository.existsById(vehicleId)) {
+            vehicleForRentalRepository.deleteById(vehicleId);
+        }
+    }
+}
+
 
 //Creare un ordine per un utente
-
-    public SaleOrder createSaleOrder(SaleOrder saleOrder, Long customerId, Long vehicleId, Long sellerId) {
-        if (idLog.getRole().equals("ADMIN")) {
-            return saleOrderRepository.save(addNewSaleOrder(saleOrder, customerId, vehicleId, sellerId));
-        } else {
-            return null;
-        }
+public SaleOrder createSaleOrder(SaleOrder saleOrder, Long customerId, Long vehicleId, Long sellerId) {
+    if (idLog.getRole().equals("ADMIN")) {
+        return saleOrderRepository.save(addNewSaleOrder(saleOrder, customerId, vehicleId, sellerId));
+    } else {
+        return null;
     }
+}
 
-    public SaleOrder addNewSaleOrder(SaleOrder saleOrder, Long customerId, Long vehicleId, Long sellerId) {
-        VehicleForSale vehicleSale = vehicleForSaleRepository.findById(vehicleId).get();
-        if (vehicleSale.getStatus().equals(StatusSale.ORDERABLE)) {
-            SaleOrder newSaleOrder = new SaleOrder();
-            newSaleOrder.setSaleOrderStatus(SaleOrderStatus.ORDER);
-            newSaleOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
-            newSaleOrder.setStatusPayment(saleOrder.getStatusPayment());
-            newSaleOrder.setVehicleId(vehicleSale);
-            newSaleOrder.setCustomerId(customerRepository.findById(customerId).get());
-            newSaleOrder.setSellerId(sellerRepository.findById(sellerId).get());
-            return newSaleOrder;
-        } else {
-            return null;
-        }
+public SaleOrder addNewSaleOrder(SaleOrder saleOrder, Long customerId, Long vehicleId, Long sellerId) {
+    VehicleForSale vehicleSale = vehicleForSaleRepository.findById(vehicleId).get();
+    if (vehicleSale.getStatus().equals(StatusSale.ORDERABLE)) {
+        SaleOrder newSaleOrder = new SaleOrder();
+        newSaleOrder.setSaleOrderStatus(SaleOrderStatus.ORDER);
+        newSaleOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
+        newSaleOrder.setStatusPayment(saleOrder.getStatusPayment());
+        newSaleOrder.setVehicleId(vehicleSale);
+        newSaleOrder.setCustomerId(customerRepository.findById(customerId).get());
+        newSaleOrder.setSellerId(sellerRepository.findById(sellerId).get());
+        return newSaleOrder;
+    } else {
+        return null;
     }
+}
+
 //Modificare un ordine per un utente
-
-    public SaleOrder updateSaleOrder(SaleOrder saleOrder, Long saleOrderId) {
-        if (idLog.getRole().equals("ADMIN")) {
-            SaleOrder newOrder = saleOrderRepository.findById(saleOrderId).get();
-            if (saleOrder.getSaleOrderStatus() != null) {
-                newOrder.setSaleOrderStatus(saleOrder.getSaleOrderStatus());
-            }
-            if (saleOrder.getSaleOrderStatement() != null) {
-                newOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
-            }
-            if (saleOrder.getStatusPayment() != null) {
-                newOrder.setStatusPayment(saleOrder.getStatusPayment());
-            }
-            return saleOrderRepository.save(newOrder);
-        } else {
-            return null;
+public SaleOrder updateSaleOrder(SaleOrder saleOrder, Long saleOrderId) {
+    if (idLog.getRole().equals("ADMIN")) {
+        SaleOrder newOrder = saleOrderRepository.findById(saleOrderId).get();
+        if (saleOrder.getSaleOrderStatus() != null) {
+            newOrder.setSaleOrderStatus(saleOrder.getSaleOrderStatus());
         }
+        if (saleOrder.getSaleOrderStatement() != null) {
+            newOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
+        }
+        if (saleOrder.getStatusPayment() != null) {
+            newOrder.setStatusPayment(saleOrder.getStatusPayment());
+        }
+        return saleOrderRepository.save(newOrder);
+    } else {
+        return null;
     }
+}
+
 //Cancellare un ordine per un utente
+public SaleOrder updateDeleted(Long saleOrderId) {
+    saleOrderRepository.updateDeletedById(saleOrderId);
+    return saleOrderRepository.findById(saleOrderId).get();
+}
 
-    public SaleOrder updateDeleted(Long saleOrderId) {
-        saleOrderRepository.updateDeletedById(saleOrderId);
-        return saleOrderRepository.findById(saleOrderId).get();
+//Creare un acquisto per un utente
+public SaleOrder createPurchase(SaleOrder saleOrder, Long sellerId, Long vehicleId, Long customerId) {
+    if (idLog.getRole().equals("ADMIN")) {
+        return saleOrderRepository.save(addNewPurchase(saleOrder, sellerId, vehicleId, customerId));
+    } else {
+        return null;
     }
+}
 
-    //Creare un acquisto per un utente
-
-    public SaleOrder createPurchase(SaleOrder saleOrder, Long sellerId, Long vehicleId, Long customerId) {
-        if (idLog.getRole().equals("ADMIN")) {
-            return saleOrderRepository.save(addNewPurchase(saleOrder, sellerId, vehicleId, customerId));
-        } else {
-            return null;
-        }
+public SaleOrder addNewPurchase(SaleOrder saleOrder, Long sellerId, Long vehicleId, Long customerId) {
+    VehicleForSale vehicleSale = vehicleForSaleRepository.findById(vehicleId).get();
+    if (vehicleSale.getStatus().equals(StatusSale.SELLABLE)) {
+        SaleOrder newSaleOrder = new SaleOrder();
+        newSaleOrder.setSaleOrderStatus(SaleOrderStatus.PURCHASE);
+        newSaleOrder.setStatusSale(saleOrder.getStatusSale());
+        newSaleOrder.setStatusPayment(saleOrder.getStatusPayment());
+        newSaleOrder.setVehicleId(vehicleSale);
+        newSaleOrder.setCustomerId(customerRepository.findById(customerId).get());
+        newSaleOrder.setSellerId(sellerRepository.findById(sellerId).get());
+        return newSaleOrder;
+    } else {
+        return null;
     }
+}
 
-    public SaleOrder addNewPurchase(SaleOrder saleOrder, Long sellerId, Long vehicleId, Long customerId) {
-        VehicleForSale vehicleSale = vehicleForSaleRepository.findById(vehicleId).get();
-        if (vehicleSale.getStatus().equals(StatusSale.SELLABLE)) {
-            SaleOrder newSaleOrder = new SaleOrder();
-            newSaleOrder.setSaleOrderStatus(SaleOrderStatus.PURCHASE);
-            newSaleOrder.setStatusSale(saleOrder.getStatusSale());
-            newSaleOrder.setStatusPayment(saleOrder.getStatusPayment());
-            newSaleOrder.setVehicleId(vehicleSale);
-            newSaleOrder.setCustomerId(customerRepository.findById(customerId).get());
-            newSaleOrder.setSellerId(sellerRepository.findById(sellerId).get());
-            return newSaleOrder;
-        } else {
-            return null;
-        }
-    }
 //Modificare un acquisto per un utente
-
-    public SaleOrder updatePurchase(SaleOrder saleOrder, Long saleOrderId) {
-        if (idLog.getRole().equals("ADMIN")) {
-            SaleOrder newOrder = saleOrderRepository.findById(saleOrderId).get();
-            if (saleOrder.getSaleOrderStatus() != null) {
-                newOrder.setSaleOrderStatus(saleOrder.getSaleOrderStatus());
-            }
-            if (saleOrder.getSaleOrderStatement() != null) {
-                newOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
-            }
-            if (saleOrder.getStatusPayment() != null) {
-                newOrder.setStatusPayment(saleOrder.getStatusPayment());
-            }
-            return saleOrderRepository.save(newOrder);
-        } else {
-            return null;
+public SaleOrder updatePurchase(SaleOrder saleOrder, Long saleOrderId) {
+    if (idLog.getRole().equals("ADMIN")) {
+        SaleOrder newOrder = saleOrderRepository.findById(saleOrderId).get();
+        if (saleOrder.getSaleOrderStatus() != null) {
+            newOrder.setSaleOrderStatus(saleOrder.getSaleOrderStatus());
         }
+        if (saleOrder.getSaleOrderStatement() != null) {
+            newOrder.setSaleOrderStatement(saleOrder.getSaleOrderStatement());
+        }
+        if (saleOrder.getStatusPayment() != null) {
+            newOrder.setStatusPayment(saleOrder.getStatusPayment());
+        }
+        return saleOrderRepository.save(newOrder);
+    } else {
+        return null;
     }
-    //Cancellare un acquisto per un utente
+}
 
-    public SaleOrder updateDeletedPurchase(Long saleOrderId) {
-        saleOrderRepository.updateDeletedById(saleOrderId);
-        return saleOrderRepository.findById(saleOrderId).get();
+//Cancellare un acquisto per un utente
+public SaleOrder updateDeletedPurchase(Long saleOrderId) {
+    saleOrderRepository.updateDeletedById(saleOrderId);
+    return saleOrderRepository.findById(saleOrderId).get();
 
-    }
+}
 
 }
 
