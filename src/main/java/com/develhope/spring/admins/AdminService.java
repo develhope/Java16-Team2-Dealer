3b89de586ac8bd616867aeee84389f82a3728a8d
Valeria Parise <parisevaleria@gmail.com>
2024-02-22 18:04:53 +0100

    create a new vehicle

--- src/main/java/com/develhope/spring/admins/AdminService.java
+++ src/main/java/com/develhope/spring/admins/AdminService.java
@@ -7,13 +7,34 @@ import com.develhope.spring.orders.forsale.SaleOrder;
 import com.develhope.spring.orders.forsale.SaleOrderRepository;
 import com.develhope.spring.orders.forsale.SaleOrderStatus;
 import com.develhope.spring.sellers.SellerRepository;
+import com.develhope.spring.vehicle.VehicleEntity;
 import com.develhope.spring.vehicle.forrental.VehicleForRentalRepository;
 import com.develhope.spring.vehicle.forsale.StatusSale;
 import com.develhope.spring.vehicle.forsale.VehicleForSale;
 import com.develhope.spring.vehicle.forsale.VehicleForSaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
+//Modificare un veicolo
+//Cancellare un veicolo
+//Cambiare lo stato di un veicolo
+//Creare un noleggio per un utente
+//Cancellare un noleggio per un utente
+//Modificare un noleggio per un utente
+
+//Verificare un venditore quante vendite ha fatto in un determinato periodo di tempo
+//Verificare un venditore quanti soldi ha generato in un determinato periodo di tempo
+//Verificare il guadagno del salone in un determinato periodo
+//Verificare i veicoli attualmente ordinabili/acquistabili/non disponibili/nuovi/usati
+//Cancellare un utente
+//Modificare un utente
+//Cancellare un venditore
+//Modificare un venditore
+//Ottenere il veicolo più venduto in un dato periodo
+//Ottenere il veicolo con valore più alto venduto fino a quel dato istante
+//Ottenere il veicolo più rircercato/ordinato
 @Service
 public class AdminService {
     @Autowired
@@ -33,6 +54,31 @@ public class AdminService {
     @Autowired
     private RentalOrderRepository rentalOrderRepository;
 
+    //    Aggiungere un veicolo
+    public VehicleForSale addVehicleSale(VehicleEntity vehicleSale) {
+        VehicleForSale vehicle = new VehicleForSale();
+        vehicle.setStatus(vehicle.getStatus());
+        vehicle.setLicensePlate(vehicle.getLicensePlate());
+        vehicle.setBrand(vehicle.getBrand());
+        vehicle.setModel(vehicle.getModel());
+        vehicle.setDisplacement(vehicle.getDisplacement());
+        vehicle.setColor(vehicle.getColor());
+        vehicle.setPower(vehicle.getPower());
+        vehicle.setTransmission(vehicle.getTransmission());
+        vehicle.setRegistrationYear(vehicle.getRegistrationYear());
+        vehicle.setEngine(vehicle.getEngine());
+        vehicle.setType(vehicle.getType());
+        vehicle.setId(vehicle.getId());
+        vehicle.setListPrice(vehicle.getListPrice());
+        vehicle.setDiscountPercentage(vehicle.getDiscountPercentage());
+        vehicle.setOptionals(vehicle.getOptionals());
+        vehicle.setIsNew(vehicle.getIsNew());
+        vehicle.setStatus(vehicle.getStatus());
+        return vehicle;
+    }
+
+
+//Creare un ordine per un utente
 
     public SaleOrder createSaleOrder(SaleOrder saleOrder, Long customerId, Long vehicleId, Long sellerId) {
         if (idLog.getRole().equals("ADMIN")) {
@@ -57,6 +103,7 @@ public class AdminService {
             return null;
         }
     }
+//Modificare un ordine per un utente
 
     public SaleOrder updateSaleOrder(SaleOrder saleOrder, Long saleOrderId) {
         if (idLog.getRole().equals("ADMIN")) {
@@ -75,11 +122,14 @@ public class AdminService {
             return null;
         }
     }
+//Cancellare un ordine per un utente
 
     public SaleOrder updateDeleted(Long saleOrderId) {
         saleOrderRepository.updateDeletedById(saleOrderId);
         return saleOrderRepository.findById(saleOrderId).get();
     }
+
+    //Creare un acquisto per un utente
 
     public SaleOrder createPurchase(SaleOrder saleOrder, Long sellerId, Long vehicleId, Long customerId) {
         if (idLog.getRole().equals("ADMIN")) {
@@ -104,6 +154,7 @@ public class AdminService {
             return null;
         }
     }
+//Modificare un acquisto per un utente
 
     public SaleOrder updatePurchase(SaleOrder saleOrder, Long saleOrderId) {
         if (idLog.getRole().equals("ADMIN")) {
@@ -122,7 +173,9 @@ public class AdminService {
             return null;
         }
     }
-    public SaleOrder updateDeletedPurchase(Long saleOrderId){
+    //Cancellare un acquisto per un utente
+
+    public SaleOrder updateDeletedPurchase(Long saleOrderId) {
         saleOrderRepository.updateDeletedById(saleOrderId);
         return saleOrderRepository.findById(saleOrderId).get();
 
