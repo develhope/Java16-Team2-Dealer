6f2448896f2d964d4375afbf1b1dac80d85de9bb
Tino <119343176+tinobks@users.noreply.github.com>
2024-02-28 16:30:17 +0100

    aggiunta getmapping getSeller singolo

--- src/main/java/com/develhope/spring/features/seller/SellerController.java
+++ src/main/java/com/develhope/spring/features/seller/SellerController.java
@@ -3,9 +3,13 @@ package com.develhope.spring.features.seller;
 
 import com.develhope.spring.features.user.dto.UserResponseDto;
 import com.develhope.spring.features.user.entities.Role;
+import com.develhope.spring.features.user.entities.UserEntity;
+import com.develhope.spring.features.user.model.UserModel;
 import com.develhope.spring.features.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,13 @@ public class SellerController {
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> getAllSellers() {
         return ResponseEntity.ok(userService.getAllByRole(Role.SELLER));
+    }
+
+    @GetMapping("/seller")
+    public ResponseEntity<UserResponseDto> getSeller(@AuthenticationPrincipal UserEntity user) {
+        UserModel model = UserModel.convertEntityToModel(user);
+        UserResponseDto dto = UserModel.convertModelToResponse(model);
+        return ResponseEntity.ok(dto);
     }
 
 }
