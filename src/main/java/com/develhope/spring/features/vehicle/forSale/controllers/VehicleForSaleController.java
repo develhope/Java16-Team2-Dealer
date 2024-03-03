a117c3513cbb531e6cbd760b659c62e2b41ec921
Fabio-Frau <fbfr91@yahoo.it>
2024-03-03 12:40:27 +0100

    aggiunta doc alle rotte precedenti

--- src/main/java/com/develhope/spring/features/vehicle/forSale/controllers/VehicleForSaleController.java
+++ src/main/java/com/develhope/spring/features/vehicle/forSale/controllers/VehicleForSaleController.java
@@ -1,7 +1,14 @@
 package com.develhope.spring.features.vehicle.forSale.controllers;
 
+import com.develhope.spring.features.user.dto.UserResponseDto;
 import com.develhope.spring.features.vehicle.forSale.dto.VehicleForSaleRequestDto;
+import com.develhope.spring.features.vehicle.forSale.dto.VehicleForSaleResponseDto;
 import com.develhope.spring.features.vehicle.forSale.service.VehicleForSaleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +21,9 @@ public class VehicleForSaleController {
 
     private final VehicleForSaleService vehicleForSaleService;
 
+    @Operation(summary = "Add a vehicle for sale")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Creates a new user with the required data",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleForSaleResponseDto.class))})})
     @PostMapping("/vehicle")
     public ResponseEntity<?> addVehicleForSale(@RequestBody VehicleForSaleRequestDto dto) {
 
@@ -21,6 +31,9 @@ public class VehicleForSaleController {
 
     }
 
+    @Operation(summary = "Get all vehicles for sale")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Get the list of all vehicles for sale",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleForSaleResponseDto.class))})})
     @GetMapping
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(vehicleForSaleService.getAll());
