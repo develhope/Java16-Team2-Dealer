319d782760315b0054ac7fa0f4d0e966d13a3185
Fabio-Frau <fbfr91@yahoo.it>
2024-03-03 16:58:07 +0100

    aggiunto saleorderError dto

---
+++ src/main/java/com/develhope/spring/features/saleOrder/dto/SaleOrderError.java
@@ -0,0 +1,18 @@
+package com.develhope.spring.features.saleOrder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleOrderError {
+
+    private int code;
+    private String message;
+
+
+}
