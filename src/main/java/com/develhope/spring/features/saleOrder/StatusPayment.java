f27329e4cfb2bf17c54a689ab68eabd862cdaa47
Fabio-Frau <fbfr91@yahoo.it>
2024-03-03 16:42:54 +0100

    creata entity SaleOrder

---
+++ src/main/java/com/develhope/spring/features/saleOrder/StatusPayment.java
@@ -0,0 +1,9 @@
+package com.develhope.spring.features.saleOrder;
+
+public enum StatusPayment {
+
+    DOWN_PAYMENT,
+    SETTLED,
+    TO_PAY
+
+}
---
+++ src/main/java/com/develhope/spring/features/saleOrder/entities/SaleOrderEntity.java
@@ -0,0 +1,54 @@
+package com.develhope.spring.features.saleOrder.entities;
+
+
+import com.develhope.spring.features.saleOrder.StatusPayment;
+import com.develhope.spring.features.user.entities.UserEntity;
+import com.develhope.spring.features.vehicleForSale.entities.VehicleForSaleEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.net.UnknownServiceException;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "sale_orders")
+public class SaleOrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    private BigDecimal downPayment;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusPayment statusPayment;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private VehicleForSaleEntity vehicle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private UserEntity customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id", nullable = false)
+    private UserEntity seller;
+
+
+}
